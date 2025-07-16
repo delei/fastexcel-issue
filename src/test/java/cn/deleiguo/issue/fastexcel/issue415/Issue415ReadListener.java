06809cc31d8068bc06883f280892881da32e5d88
DeleiGuo <deleiguo@163.com>
2025-07-16 14:14:28 +0800

    refactor(fastexcel): update Issue415ReadListener to implement ReadListener

--- src/test/java/cn/deleiguo/issue/fastexcel/issue415/Issue415ReadListener.java
+++ src/test/java/cn/deleiguo/issue/fastexcel/issue415/Issue415ReadListener.java
@@ -1,8 +1,8 @@
 package cn.deleiguo.issue.fastexcel.issue415;
 
 import cn.idev.excel.context.AnalysisContext;
-import cn.idev.excel.event.AnalysisEventListener;
 import cn.idev.excel.metadata.CellExtra;
+import cn.idev.excel.read.listener.ReadListener;
 import com.alibaba.fastjson2.JSON;
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
  * @author deleiguo
  */
 @Slf4j
-public class Issue415ReadListener extends AnalysisEventListener<ProductImportReqVO> {
+public class Issue415ReadListener implements ReadListener<ProductImportReqVO> {
     @Override
     public void invoke(ProductImportReqVO data, AnalysisContext context) {
         log.info("解析到一条数据:{}", JSON.toJSONString(data));
